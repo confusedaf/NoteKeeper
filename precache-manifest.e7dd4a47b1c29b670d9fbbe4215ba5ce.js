7b7dc4f6265e68757deebe8b29520a2765f3c90a
Aswathy <aswathys2k@gmail.com>
2020-12-11 16:09:17 +0530

    Updates

---
+++ precache-manifest.e7dd4a47b1c29b670d9fbbe4215ba5ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbea6deb973b68f54badb57fd8fb4d16",
+    "url": "/NoteKeeper/index.html"
+  },
+  {
+    "revision": "f8d207f43e7a7b0c5f52",
+    "url": "/NoteKeeper/static/js/2.51e4bc8c.chunk.js"
+  },
+  {
+    "revision": "e0b061db1880be342a43",
+    "url": "/NoteKeeper/static/js/main.0bf28e06.chunk.js"
+  },
+  {
+    "revision": "1e625f02af0674466dd8",
+    "url": "/NoteKeeper/static/js/runtime-main.0155673d.js"
+  }
+]);
